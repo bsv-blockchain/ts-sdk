d21b178b488e915423f6692016778e2264679718
Ty J Everett <ty@tyweb.us>
2026-01-21 12:07:20 -0800

    initial

--- src/script/ScriptChunk.ts
+++ src/script/ScriptChunk.ts
@@ -1,7 +1,17 @@
 /**
  * A representation of a chunk of a script, which includes an opcode. For push operations, the associated data to push onto the stack is also included.
  */
+export interface ScriptMacroTag {
+  name: string
+  length: number
+  role: 'start' | 'body'
+  index: number
+  ownerId?: number
+  meta?: Record<string, unknown>
+}
+
 export default interface ScriptChunk {
   op: number
   data?: number[]
+  macro?: ScriptMacroTag
 }
